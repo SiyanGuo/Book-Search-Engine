e4481841f6ad0dc2800192d0f7b159e67261fa99
Siyan Guo <siyang@Hey-There.local>
2021-11-07 19:27:48 -0500

    update saveBook functionality

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -2,8 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+// import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { SAVE_BOOK } from '../utils/mutations';
+import { useMutation } from '@apollo/client';
+
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -13,6 +16,8 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+
+  const [saveBook, { error }] = useMutation(SAVE_BOOK)
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -58,18 +63,22 @@ const SearchBooks = () => {
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
     // get token
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
+    // const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-    if (!token) {
-      return false;
-    }
+    // if (!token) {
+    //   return false;
+    // }
+
+    const {bookId,title,link,image,description,authors} = bookToSave;
 
     try {
-      const response = await saveBook(bookToSave, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      // const response = await saveBook(bookToSave, token);
+      await saveBook({
+        variables: { bookToSave,title,link,image,description,authors }
+    });
+      // if (!response.ok) {
+      //   throw new Error('something went wrong!');
+      // }
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -24,13 +24,13 @@ export const ADD_USER = gql`
   }
 `;
 
-
 export const SAVE_BOOK = gql`
-  mutation saveBook() {
-    saveBook(thoughtId: $thoughtId, reactionBody: $reactionBody) {
+  mutation saveBook($authors:[String]!,$description:String!,$title:String!,$bookId:String!,$image:String!,$link:String!) {
+    saveBook(authors: $authors, description: $description, title:$title, bookId:$bookId, image:$image, link:$link) {
       _id
       username
-      saveBooks {
+      bookCount
+      savedBooks {
         _id
         authors
         description
@@ -39,6 +39,25 @@ export const SAVE_BOOK = gql`
         image
         link
       }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+        _id
+        username
+        bookCount
+        savedBooks {
+          _id
+          authors
+          description
+          title
+          bookId
+          image
+          link
+        }
     }
   }
 `;
